d1cdea9f620b8e3e16f8515d532dfaac501cc322
Preeti <preetiyadav5443@gmail.com>
2025-09-19 20:55:44 +0530

    add .env

--- src/lib/sanityClient.js
+++ src/lib/sanityClient.js
@@ -1,11 +1,11 @@
 import { createClient } from "next-sanity";
 
 export const client = createClient({
-  projectId: "n4w8o5wo",
-  dataset: "production",
+  projectId: process?.env?.NEXT_PUBLIC_SANITY_PROJECT_ID || "n4w8o5wo",
+  dataset: process?.env?.NEXT_PUBLIC_SANITY_DATASET || "production",
   useCdn: true,
-  apiVersion: '2023-05-03',
-  token: 'skq4MewCYqhZI7Ibjf0e6YqtK7cTfPZ72xOyxJ9dQZfNCmzqmwv1VFgsukEznP3l6nASHeSBPQFOxyJO27aaKM4H2MbXqEBqJ9pNANaSTEtXZmASy9BrUpkT60P8H71IaTWSU6wxdvOtls0lzJg3UMgMtl5Zgiv6gFIVmMHkSO3sDZAJT9QW',
+  apiVersion: "2023-05-03",
+  token: process?.env?.SANITY_API_TOKEN,
 });
 
 
